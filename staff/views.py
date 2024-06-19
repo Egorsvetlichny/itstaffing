e681375dc7bc48f7af2d965403b9023dec2adcf9
EgorSvetlichnyy <fkhor@inbox.ru>
2024-06-19 13:20:26 +0300

    Create base.html and extend index.html from it

--- staff/views.py
+++ staff/views.py
@@ -7,4 +7,5 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        context['body'] = 'Содержимое главной страницы'
         return context
