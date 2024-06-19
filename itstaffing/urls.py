cfe01255614bf47f817acb1b91eead2ae76ccf6c
EgorSvetlichnyy <fkhor@inbox.ru>
2024-06-19 14:09:45 +0300

    Add main page handler

--- itstaffing/urls.py
+++ itstaffing/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from staff.views import HomePageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view())
 ]
