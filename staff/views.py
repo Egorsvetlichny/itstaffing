0157d59dd3fa918f1354824df29ba5874d0e4a70
Egor Svetlichny <fkhor@inbox.ru>
2024-06-25 20:14:10 +0300

    Document class HomePageView in views.py

--- staff/views.py
+++ staff/views.py
@@ -2,9 +2,15 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
+    """
+    A view that displays the home page.
+    """
     template_name = 'staff/index.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Adds custom context data to the view.
+        """
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
         context['body'] = 'Содержимое главной страницы'
