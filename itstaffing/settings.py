84056d6a45824dab10bf6a6710784fd9ef1a9ef0
Egor Svetlichny <fkhor@inbox.ru>
2024-07-01 16:43:40 +0300

    Correctly configure the database for tests and added some tests for the main page

--- itstaffing/settings.py
+++ itstaffing/settings.py
@@ -91,10 +91,6 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- staff/tests.py
+++ staff/tests.py
@@ -1,15 +1,44 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 import unittest
+from unittest.mock import patch
+from django.test import RequestFactory
+from django.urls import reverse
+from staff.views import HomePageView
 
 
 class HomePageViewTest(TestCase):
-    # def setUp(self):
-    #     """Создает тестовый клиент перед каждым тестом."""
-    #     self.client = self.itstaffing.test_client()
+    def setUp(self):
+        """Создает тестовый клиент перед каждым тестом."""
+        self.factory = RequestFactory()
 
     def test_view_url_exists_at_desired_location(self):
+        """Test that the url exists at the desired location."""
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        """Test that the url is accessible by name."""
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """Test that the view uses the correct template."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'staff/index.html')
+
+    def test_view_returns_correct_content(self):
+        """Test that the view returns the correct content."""
+        request = self.factory.get('/')
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Главная страница')
+        self.assertContains(response, 'Содержимое главной страницы')
+
+    def test_view_returns_404_for_nonexistent_url(self):
+        """Test that the view returns a 404 for a nonexistent url."""
+        response = self.client.get('/nonexistent-url/')
+        self.assertEqual(response.status_code, 404)
 
 
 if __name__ == '__main__':
