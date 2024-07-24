a8a0ea18ca7312ad0bb265f3359c85bf60613a8d
Egor Svetlichny <fkhor@inbox.ru>
2024-07-24 15:19:08 +0300

    create model Company and register it

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from staff.models import Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'email', 'website_url', 'industry', 'location', 'logo_of_company',
+                    'logo_of_company_small',)
+    list_display_links = ('id', 'name')
+    list_per_page = 6
