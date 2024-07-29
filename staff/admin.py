2fcf6818ce4315f0c2e75212b706045490875717
Egor Svetlichny <fkhor@inbox.ru>
2024-07-29 22:08:50 +0300

    add new model Vacancy and register it

--- staff/admin.py
+++ staff/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from staff.models import Company
+from staff.models import Company, Vacancy
 
 
 @admin.register(Company)
@@ -8,6 +8,15 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'email', 'website_url', 'industry', 'location', 'logo_of_company',
                     'logo_of_company_small',)
     list_display_links = ('id', 'name')
+    list_per_page = 6
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'speciality', 'description', 'company', 'location', 'salary',
+                    'published_date', 'benefits', 'skills', 'type_of_employment', 'url_for_vacancy', 'contact_email',
+                    'contact_phone', 'logo_of_vacancy', 'logo_of_vacancy_small',)
+    list_display_links = ('id', 'title')
     list_per_page = 6
 
 
