31ae1f7f1fe3a7279affc12c34280322e110d4ac
Egor Svetlichny <fkhor@inbox.ru>
2024-07-30 13:52:05 +0300

    Change fields properties in model Vacancy

--- staff/models.py
+++ staff/models.py
@@ -24,22 +24,22 @@ class Company(models.Model):
 
 
 class Vacancy(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Вакансия')
-    slug = models.SlugField(max_length=255, verbose_name='Слаг')
+    title = models.CharField(max_length=255, db_index=True, verbose_name='Вакансия')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Слаг')
     speciality = models.CharField(max_length=255, verbose_name='Специальность')
-    description = models.TextField(verbose_name='Описание вакансии')
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='Компания')
-    location = models.CharField(max_length=255, verbose_name='Расположение')
-    salary = models.IntegerField(verbose_name='Зарплата')
+    description = models.TextField(blank=True, verbose_name='Описание вакансии')
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, verbose_name='Компания')
+    location = models.CharField(max_length=255, blank=True, verbose_name='Расположение')
+    salary = models.IntegerField(blank=True, verbose_name='Зарплата')
     published_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     benefits = models.TextField(blank=True, verbose_name='Преимущества')
-    skills = models.JSONField(blank=True, verbose_name='Навыки')
-    type_of_employment = models.CharField(max_length=255, blank=True, verbose_name='Тип работы')
-    url_for_vacancy = models.URLField(max_length=255, blank=True, verbose_name='Ссылка на вакансию')
-    contact_email = models.EmailField(max_length=255, blank=True, verbose_name='Контактный email')
-    contact_phone = models.CharField(max_length=255, blank=True, verbose_name='Контактный телефон')
-    logo_of_vacancy = models.ImageField(upload_to='logos', blank=True, verbose_name='Лого')
-    logo_of_vacancy_small = models.ImageField(upload_to='logos', blank=True, verbose_name='Миниатюра лого')
+    skills = models.JSONField(blank=True, verbose_name='Необходимые навыки')
+    type_of_employment = models.CharField(max_length=255, blank=True, verbose_name='Вид трудоустройства')
+    url_for_vacancy = models.URLField(max_length=255, blank=True, unique=True, verbose_name='Ссылка на вакансию')
+    contact_email = models.EmailField(max_length=255, unique=True, verbose_name='Контактный email')
+    contact_phone = models.CharField(max_length=255, unique=True, verbose_name='Контактный телефон')
+    logo_of_vacancy = models.ImageField(upload_to='logos', blank=True, unique=True, verbose_name='Лого')
+    logo_of_vacancy_small = models.ImageField(upload_to='logos', blank=True, unique=True, verbose_name='Миниатюра лого')
 
     class Meta:
         verbose_name = 'Вакансия'
